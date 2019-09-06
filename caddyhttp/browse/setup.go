1d5221118cf42b8b1a9979712722cfbf594aca33
Conrado Quilles Gomes <conradoqg@gmail.com>
2019-09-06 19:26:03 -0300

    Hide files and folders starting with dot

--- caddyhttp/browse/setup.go
+++ caddyhttp/browse/setup.go
@@ -485,6 +485,7 @@ footer {
 					</tr>
 					{{- end}}
 					{{- range .Items}}
+					{{if ne ($.Truncate .Name 1) "."}}
 					<tr class="file">
 						<td></td>
 						<td>
@@ -505,6 +506,7 @@ footer {
 						<td class="hideable"><time datetime="{{.HumanModTime "2006-01-02T15:04:05Z"}}">{{.HumanModTime "01/02/2006 03:04:05 PM -07:00"}}</time></td>
 						<td class="hideable"></td>
 					</tr>
+					{{end}}
 					{{- end}}
 					</tbody>
 				</table>
